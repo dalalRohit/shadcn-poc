fb2737051e69d2111b779a58c4a243420df1a434
Rohit Dalal <dalalrohit102@gmail.com>
2023-06-12 13:01:05 +0530

    setup poc

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Button, buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
@@ -17,6 +23,7 @@ export default function IndexPage() {
         </p>
       </div>
       <div className="flex gap-4">
+        <Button variant="ghost">Test Button</Button>
         <Link
           href={siteConfig.links.docs}
           target="_blank"
@@ -34,6 +41,29 @@ export default function IndexPage() {
           GitHub
         </Link>
       </div>
+      <br />
+      <h1 className="text-lg">Accordion Demo</h1>
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other components
+            aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. Its animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </section>
   )
 }
